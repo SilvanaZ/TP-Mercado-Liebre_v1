e0dd2fcd076966b0a40f4d9d73d93917869f6107
silvana zalazar <silvana.blaq@gmail.com>
2021-11-15 18:33:26 -0300

    cambios de ML

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3030
+const path = require('path')
+
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "views/index.html"))
+})
+
+app.listen(PORT, () => console.log('Servidor Levantado'));
