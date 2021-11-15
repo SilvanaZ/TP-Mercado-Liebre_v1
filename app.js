8fb6c9d7003de00af1c464cb59a4e394bb73db77
silvana zalazar <silvana.blaq@gmail.com>
2021-11-15 20:08:22 -0300

    cambios y rehaciendo ML

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 3030
+const express = require('express');
+let app = express();
+const PORT = 3030;
 const path = require('path')
-
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "views/index.html"))
 })
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, "views/register.html"))
+})
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, "views/login.html"))
+})
 
-app.listen(PORT, () => console.log('Servidor Levantado'));
+app.listen(PORT, () => console.log(`El servidor esta escuchando en el
+puerto ${PORT}
+http://localhost:${PORT}`));
