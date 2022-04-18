50a58517c2d26d805a74885bcf97255ac1b80e35
Silvana Zalazar <90283693+SilvanaZ@users.noreply.github.com>
2022-04-18 17:38:14 -0300

    Changes
    Making changes, hoover, colors and more :D

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
 let express = require('express');
 let app = express();
-const PORT = 3000;
-let path = require('path')
+const PORT = 8001;
+let path = require('path');
 
 //middlewares
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-// Routes
-app.get('/', function (req, res){
-    res.sendFile(path.join(__dirname, './views/home.html'))
+//Routes-rutas
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '/views/home.html')) //write permite varias lineas el send corta la ejecucuion de otras lineas
+    //res.write - res.end()cortamos
 })
 
-//Server
-app.listen(PORT, () => console.log(`
-Servidor escuchando en el puerto ${PORT}
-http://localhost:${PORT}
-`))
+app.get('/register', function (req, res) {
+    res.sendFile(path.join(__dirname, '/views/register.html'))
+})
+
+app.get('/login', function (req, res) {
+    res.sendFile(path.join(__dirname, '/views/login.html'))
+})
+
+
+
+//Server-servidor
+app.listen(PORT, () => console.log(`Servidor escuchando en el puerto ${PORT} -> http://localhost:${PORT}`))
